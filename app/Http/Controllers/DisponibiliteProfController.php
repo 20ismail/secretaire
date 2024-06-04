bf8953d27685e9e93622b7058c5572259976995e
sfdhafsa <hafsasifeddine8@gmail.com>
2024-06-04 14:54:36 +0100

    dis

--- app/Http/Controllers/DisponibiliteProfController.php
+++ app/Http/Controllers/DisponibiliteProfController.php
@@ -8,6 +8,7 @@ use App\Models\Professeur;
 use Illuminate\Support\Facades; // Import DB facade from correct namespace
 use App\Models\Semestre;
 use App\Models\Secretaire;
+use App\Models\Departement;
 
 class DisponibiliteProfController extends Controller
 {
@@ -21,43 +22,29 @@ class DisponibiliteProfController extends Controller
     // Handle form submission to get professor availability based on selected semester
     public function index(Request $request)
     {
+        // Fetch all semesters
         $semestres = Semestre::all();
-        $disponibilites = [];
 
-        $selectedSemestre = $request->input('select-semestre');
-        
         // Assuming you have the authenticated secretary
         $secretaire = auth()->user();
-        $departmentIntitule = $secretaire->departement;
+        $departementIntitule = $secretaire->departement;
 
-        if ($selectedSemestre) {
-            $semestre = Semestre::where('numero_semestre', $selectedSemestre)->first();
+        // Fetch the department based on the secretary's department
+        $departement = Departement::where('intitule', $departementIntitule)->first();
 
-            if ($semestre) {
-                $department = Department::where('intitule', $departmentIntitule)->first();
-
-                if ($department) {
-                    $disponibilites = DisponibiliteProf::where('id_semestre', $semestre->id)
-                        ->whereHas('professeur', function($query) use ($department) {
-                            $query->where('department_id', $department->id);
-                        })
-                        ->with('professeur')
-                        ->get();
-                }
-            }
+        if ($departement) {
+            // Fetch all availabilities for the department's professors
+            $disponibilites = DisponibiliteProf::whereHas('professeur', function($query) use ($departement) {
+                    $query->where('departement_id', $departement->id);
+                })
+                ->with(['professeur', 'semestre'])
+                ->get();
+        } else {
+            $disponibilites = [];
         }
-        return view('Secretaire.DispoEnseignant', compact('semestres', 'selectedSemestre', 'disponibilites'));
+
+        return view('Secretaire.DispoEnseignant', compact('semestres', 'disponibilites'));
     }
-
-                
-            
-
-
-
-
-
-
-
 
     /**
      * Show the form for creating a new resource.
